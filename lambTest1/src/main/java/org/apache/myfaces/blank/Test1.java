a69921ea56a6eb69fd4c64f1696dfc64039801ee
daihang <daihang@hellogeek.com>
2019-05-09 15:48:02 +0800

    init

---
+++ lambTest1/src/main/java/org/apache/myfaces/blank/Test1.java
@@ -0,0 +1,13 @@
+package org.apache.myfaces.blank;
+
+/**
+ * @Created with IDEA
+ * @author:DaiHang
+ * @Date:2019/5/9
+ * @Time:15:43
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
